05cd9c6f669dae374fbfa896fec9faf662a9a611
OanaVasilescu <vasilescu.oana28@gmail.com>
2021-03-31 19:41:54 +0300

    added simple toolbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './authentification/login/login.component';
+import { RegisterComponent } from './authentification/register/register.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './shared/navigation/navbar/navbar.component';
+import { LessonComponent } from './lesson/lesson.component';
+import { ChatComponent } from './chat/chat.component';
+import { QuizComponent } from './quiz/quiz.component';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    NavbarComponent,
+    LessonComponent,
+    ChatComponent,
+    QuizComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
