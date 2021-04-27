25dacdbbe48ab10f70ee5bcfdddc29b145602f47
OanaVasilescu <vasilescu.oana28@gmail.com>
2021-04-28 02:14:36 +0300

    fix float right problem

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,6 +9,16 @@ export class HomeComponent implements OnInit {
 
   constructor() { }
 
+    tiles = [
+      {text: 'descriere', cols: 3, rows: 4, color: 'lightblue'},
+      {text: 'you can chat', cols: 1, rows: 7, color: 'lightgreen'},
+      {text: 'you can take lessons', cols: 1, rows: 6, color: 'lightpink'},
+      {text: 'you can take quizes', cols: 2, rows: 3, color: '#DDBDF1'},
+      {text: 'extra2', cols: 1, rows: 3, color: '#Cdeaff'},
+      {text: 'extra3', cols: 2, rows: 3, color: '#F5d5d4'},
+      
+    ];
+
   ngOnInit(): void {
     console.log("ai ajuns in home");
   }
