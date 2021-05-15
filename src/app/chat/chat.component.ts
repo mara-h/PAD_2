c085ed9c3709c530cc33ef24275d72ab27bc88ef
Alexandra Glentoaica <aglentoaica@gmail.com>
2021-05-15 15:21:28 +0300

    Added websocket service

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,13 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import {ChatService} from '../../app/shared/service/webSocket/chat.service';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
-  styleUrls: ['./chat.component.css']
+  styleUrls: ['./chat.component.css'],
+  providers:[ChatService]
 })
 export class ChatComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _chatService:ChatService){}
 
   ngOnInit(): void {
   }
---
+++ src/app/shared/service/webSocket/chat.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import io from 'socket.io-client';
+import { Observable, Subject, from } from 'rxjs';
+
+
+@Injectable()
+
+export class ChatService{
+    private socket=io('http://localhost:8080'); 
+
+}
